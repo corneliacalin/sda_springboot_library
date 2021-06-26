3905e97a0e2d67a84fcca6dbdfa01b76e4b17036
Cornelia Calin <corneliacalin@yahoo.com>
2021-06-26 16:50:02 +0300

    add initial git boot structure (first step)

---
+++ src/main/java/com/sda/corneliacalin/onlinelibrary/OnlinelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sda.corneliacalin.onlinelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinelibraryApplication.class, args);
+	}
+
+}
